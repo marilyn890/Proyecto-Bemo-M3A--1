ba8328a0c1fe5aef414869a6c4813fa5fb668112
darkay <102825899+carlosmero1996@users.noreply.github.com>
2022-09-06 16:27:14 -0500

    creacion del paquete modelo conexion2

---
+++ src/modelo/MConexion/Modelo_PedidoPastel.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.MConexion;
+
+import modelo.PedidoPastel;
+
+/**
+ *
+ * @author carlo
+ */
+public class Modelo_PedidoPastel extends PedidoPastel{
+    
+}
